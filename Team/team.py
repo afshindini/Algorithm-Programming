5d2c1a78ace294a8d4484ce3714e7867176a17ee
afshindini <afshin_dini@yahoo.com>
2024-10-19 17:49:21 +0300

    update team code

--- Team/team.py
+++ Team/team.py
@@ -1,21 +1,14 @@
 """Team problem"""
 
-
 def team_decision() -> int:
     """Return the number of solved solutions by the team"""
-    res = 0
+    result = 0
     num = int(input("Enter the number of problems: "))
-    while(num>0):
-        if (input("Enter results separated by space:").count("1")>1):
-            res +=1
+    while num>0:
+        if input("Enter results separated by space:").count("1")>1:
+            result +=1
         num -=1
-    return res
-
+    return result
 
 if __name__ == "__main__":
-    res = team_decision()
-    print(res)
-    
-
-
-
+    print(team_decision())
