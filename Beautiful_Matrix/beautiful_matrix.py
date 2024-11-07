292a704fef2a22f36bcbc3c52eb97285326d5adb
Afshin Dini <afshin_dini@yahoo.com>
2024-11-08 00:24:37 +0200

    minor modification

--- Beautiful_Matrix/beautiful_matrix.py
+++ Beautiful_Matrix/beautiful_matrix.py
@@ -1,10 +1,10 @@
 """This is a solution for beautiful matrix problem"""
 
 def beautiful_matrix() -> int:
-    """Return the number of contestants for next round"""
+    """Return the number of moves to make matrix beautiful"""
     row_id, col_id = 0, 0
     for idx in range(5):
-        row = input("").split(" ")
+        row = input("Elements of the one row but 5 times separated by comma: ").split(" ")
         if '1' in row:
             row_id =  idx + 1
             col_id = row.index('1') + 1
