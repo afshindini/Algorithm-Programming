7f95b97094dea43e8f8ecefabde1cd729ae33c7e
Afshin Dini <afshin_dini@yahoo.com>
2024-11-07 22:27:13 +0200

    next round modification

--- Next_Round/next_round.py
+++ Next_Round/next_round.py
@@ -4,7 +4,7 @@ def next_round() -> int:
     """Return the number of contestants for next round"""
     _, place = map(int, input("Enter the number of contestants and kth-place seperated by space: ").split(" "))
     scores = list(map(int,input("Enter n scores seperated by space: ").split(" ")))
-    return sum(1 if score >= scores[place-1] and score >0 else 0 for score in scores )
+    return sum(score >= scores[place-1] and score >0 for score in scores )
 
 
 if __name__ == "__main__":
