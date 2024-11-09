6495d1b3ccaee47d38cc9c0332777caa2c747486
Afshin Dini <afshin_dini@yahoo.com>
2024-11-09 23:39:42 +0200

    Word capitalization minor modification

--- Word_Capitalization/word_capitalization.py
+++ Word_Capitalization/word_capitalization.py
@@ -3,10 +3,9 @@
 
 def word_capitalization() -> str:
     """Return the first letter in uppercase and keep other ones unchanged"""
-    word = input()
+    word = input("Enter the word: ")
     
     return word[0].upper() + word[1:]
-
 
 
 if __name__ == "__main__":
