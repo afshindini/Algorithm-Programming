296b12d769056672516f9727d41d4d675b872401
Afshin Dini <afshin_dini@yahoo.com>
2024-11-09 23:25:29 +0200

    Helpful math problem

---
+++ Helpful_Maths/helpful_maths.py
@@ -0,0 +1,14 @@
+"""This is a solution for helpful maths problem"""
+
+
+def helpful_maths() -> str:
+    """Return the math equation in non increasing order"""
+    eq = list(input("Enter the sum equation only containing 1/2/3/+: ").split("+"))
+    eq.sort()
+    return "+".join(eq)
+
+
+
+if __name__ == "__main__":
+    print(helpful_maths())
+    
