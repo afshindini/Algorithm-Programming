ab271855921e745aa53c110cf37e1fcd65f4c48b
Afshin Dini <afshin_dini@yahoo.com>
2024-11-10 10:57:37 +0200

    Minor modification

--- Big_Brother/big_brother.py
+++ Big_Brother/big_brother.py
@@ -2,7 +2,7 @@
 import math
 
 def big_brother() -> int:
-    """Return the math equation in non increasing order"""
+    """Find the years to be big enough"""
     weights = list(map(int, input("Enter the weights separated by space: ").split(" ")))
     return int(math.log(weights[1]/weights[0])/math.log(1.5))+1
 
