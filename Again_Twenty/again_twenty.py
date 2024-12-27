e8a5bbda36dad520ce11c9b2a69f54901e838d7b
Afshin Dini <afshin_dini@yahoo.com>
2024-12-28 01:15:02 +0200

    Solution to Again Twenty Five problem

---
+++ Again_Twenty/again_twenty.py
@@ -0,0 +1,10 @@
+"""This is a solution for again twenty five problem"""
+
+def again_twenty() -> int:
+    """Return two last digits of 5^n"""
+    _ = int(input())
+    return 25
+
+
+if __name__ == "__main__":
+    print(again_twenty())
